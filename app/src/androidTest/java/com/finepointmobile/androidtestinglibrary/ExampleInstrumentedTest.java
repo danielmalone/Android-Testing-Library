2653e3f64cdcf2a2365f415b8bb45e7ea7c8d40f
Daniel Malone <danieljmalone@gmail.com>
2017-07-27 17:07:01 -0500

    make espresso test work

--- app/src/androidTest/java/com/finepointmobile/androidtestinglibrary/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/finepointmobile/androidtestinglibrary/ExampleInstrumentedTest.java
@@ -2,11 +2,17 @@ package com.finepointmobile.androidtestinglibrary;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,11 +22,18 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class, true, true);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.finepointmobile.androidtestinglibrary", appContext.getPackageName());
+
+        Espresso.onView(withId(R.id.first_name)).perform(click());
+        Espresso.onView(withId(R.id.first_name)).perform(typeText("Daniel Malone is awesome!"));
     }
 }
